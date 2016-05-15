38e5eba6fd57d56b390d757d86ffdd5ec28a2a8e
Israel Gomez <israelgomez93@hotmail.com>
2016-05-15 15:04:39 -0500

    Tweets

---
+++ VisualizadorTweets/VisualizadorTweets/Controllers/TweetsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+using System.Threading.Tasks;
+using VisualizadorTweets.Models;
+
+namespace VisualizadorTweets.Controllers
+{
+    public class TweetsController : ApiController
+    {
+        HBaseReader hbase = new HBaseReader();
+
+        public async Task<IEnumerable<Tweet>> GetTweetsByQuery(string query)
+        {
+            return await hbase.QueryTweetsByKeywordAsync(query);
+        }
+    }
+}
---
+++ VisualizadorTweets/VisualizadorTweets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VisualizadorTweets.Startup))]
+namespace VisualizadorTweets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
